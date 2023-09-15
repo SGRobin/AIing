e44c3f05b1efde3a1c17ab00797f7129eb0b9fd8
SG_Robin <shlomogdd@gmail.com>
2023-09-15 17:47:58 +0300

    new project

---
+++ main.py
@@ -0,0 +1,12 @@
+import gymnasium as gym
+env = gym.make("LunarLander-v2", render_mode ="human")
+
+observation, info = env.reset(seed=42)
+for _ in range(100):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    env.render()
+
+    if terminated or truncated:
+        observation, info = env.reset()
+env.close()
